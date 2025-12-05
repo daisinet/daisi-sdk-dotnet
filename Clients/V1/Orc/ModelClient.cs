32effe75ecb9006b019a463b5918b196eab6918c
David Graham <22407691+OriginalCoder@users.noreply.github.com>
2025-12-05 12:41:04 -0600

    Remove s in Models to be consistent with other clients

--- Clients/V1/Orc/ModelClient.cs
+++ Clients/V1/Orc/ModelClient.cs
@@ -16,14 +16,14 @@ namespace Daisi.SDK.Clients.V1.Orc
         {
 
         }
-        public ModelsClient Create(string? orcDomainOrIp = default, int? orcPort = null)
+        public ModelClient Create(string? orcDomainOrIp = default, int? orcPort = null)
         {
-            return new ModelsClient(orcDomainOrIp ?? DaisiStaticSettings.OrcIpAddressOrDomain, orcPort ?? DaisiStaticSettings.OrcPort, clientKeyProvider);
+            return new ModelClient(orcDomainOrIp ?? DaisiStaticSettings.OrcIpAddressOrDomain, orcPort ?? DaisiStaticSettings.OrcPort, clientKeyProvider);
         }
     }
-    public partial class ModelsClient : ModelsProto.ModelsProtoClient
+    public partial class ModelClient : ModelsProto.ModelsProtoClient
     {
-        private ModelsClient(GrpcChannel orcChannel, IClientKeyProvider clientKeyProvider)
+        private ModelClient(GrpcChannel orcChannel, IClientKeyProvider clientKeyProvider)
            : base(orcChannel.Intercept((metadata) =>
            {
                if (clientKeyProvider is not null)
@@ -36,7 +36,7 @@ namespace Daisi.SDK.Clients.V1.Orc
         {
 
         }
-        internal ModelsClient(string orcDomainOrIp, int orcPort, IClientKeyProvider clientKeyProvider)
+        internal ModelClient(string orcDomainOrIp, int orcPort, IClientKeyProvider clientKeyProvider)
             : this(GrpcChannel.ForAddress($"{(DaisiStaticSettings.OrcUseSSL ? "https" : "http")}://{orcDomainOrIp}:{orcPort}"), clientKeyProvider)
         {
 
