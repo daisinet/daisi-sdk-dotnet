9be94497cb55d1d187046fc49ade4213aecf1a3b
David Graham <22407691+OriginalCoder@users.noreply.github.com>
2025-12-12 14:58:18 -0600

    general information working

---
+++ Clients/V1/Orc/AccountClient.cs
@@ -0,0 +1,44 @@
+﻿using Daisi.Protos.V1;
+using Daisi.SDK.Interfaces;
+using Daisi.SDK.Models;
+using Grpc.Core.Interceptors;
+using Grpc.Net.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Daisi.SDK.Clients.V1.Orc
+{
+    public class AccountClientFactory(IClientKeyProvider clientKeyProvider)
+    {
+        public AccountClientFactory() : this(DaisiStaticSettings.DefaultClientKeyProvider) { }
+
+        public AccountClient Create(string? orcDomainOrIp = default, int? orcPort = null)
+        {
+            return new AccountClient(orcDomainOrIp ?? DaisiStaticSettings.OrcIpAddressOrDomain, orcPort ?? DaisiStaticSettings.OrcPort, clientKeyProvider);
+        }
+    }
+    public class AccountClient : AccountsProto.AccountsProtoClient
+    {
+
+        AccountClient(GrpcChannel orcChannel, IClientKeyProvider clientKeyProvider)
+            : base(orcChannel.Intercept((metadata) =>
+            {
+                if (clientKeyProvider is not null)
+                    metadata.Add(DaisiStaticSettings.ClientKeyHeader, clientKeyProvider.GetClientKey());
+                else
+                    metadata.Add(DaisiStaticSettings.ClientKeyHeader, DaisiStaticSettings.ClientKey);
+
+                return metadata;
+            }))
+        {
+
+
+        }
+
+        internal AccountClient(string orcDomainOrIp, int orcPort, IClientKeyProvider clientKeyProvider)
+            : this(GrpcChannel.ForAddress($"{(DaisiStaticSettings.OrcUseSSL ? "https" : "http")}://{orcDomainOrIp}:{orcPort}"), clientKeyProvider)
+        {
+        }
+    }
+}
---
+++ Extensions/GetHostsRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Daisi.Protos.V1
+{
+    public partial class GetHostsRequest
+    {
+        partial void OnConstruction()
+        {
+            this.Paging = new();
+        }
+    }
+}
