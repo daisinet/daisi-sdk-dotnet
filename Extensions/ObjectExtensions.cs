f2881c592f4531d014ca298bccc2f5208df182d3
David Graham <22407691+OriginalCoder@users.noreply.github.com>
2025-12-18 16:47:50 -0600

    piping for the networks management page

---
+++ Extensions/ObjectExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Daisi.SDK.Extensions
+{
+    public static class ObjectExtensions
+    {
+        extension(Object from)
+        {
+            /// <summary>
+            /// Copies this object's properties of the same name and type to the given object
+            /// </summary>
+            /// <typeparam name="TTo">Any object that is a class.</typeparam>
+            /// <param name="toObj">The object that will receive the new values.</param>
+            /// <returns>The To object that was originally given as a parameter with new values where applicable.</returns>
+            public TTo CopyTo<TTo>(TTo toObj)
+                where TTo : class
+            {
+                if (toObj is null) return null;
+
+                var toProps = toObj.GetType().GetProperties().Where(p => p.CanWrite).ToDictionary(p => p.Name, p => p);
+                var fromProps = from.GetType().GetProperties().Where(p => p.CanRead);
+
+                foreach (var fromProp in fromProps)
+                {
+                    if (toProps.TryGetValue(fromProp.Name, out var toProp)
+                        && toProp.PropertyType == fromProp.PropertyType)
+                    {
+                        toProp.SetValue(toObj, fromProp.GetValue(from, null));
+                    }
+                }
+
+                return toObj;
+            }
+        }
+    }
+}
