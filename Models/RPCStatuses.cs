5e472a378845dfbcf9047ae505f0ea6f9e0e00b1
David Graham <22407691+OriginalCoder@users.noreply.github.com>
2025-12-23 17:06:30 -0600

    bad client authorization. needs server validation.

---
+++ Models/RPCStatuses.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Daisi.Orc.Core.Data.Models
+{
+    public class RPCStatuses
+    {
+        public static Status InvalidAccount = new Status(StatusCode.InvalidArgument, "Invalid Account");
+    }
+}
