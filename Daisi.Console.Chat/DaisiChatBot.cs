6306db6889560b413e2242c1844ecc0bc85a687b
OriginalCoder <22407691+OriginalCoder@users.noreply.github.com>
2026-01-24 12:55:18 -0600

    fixed up solution with other projects and referenced projects instead of sdk in nuget

---
+++ Daisi.Console.Chat/DaisiChatBot.cs
@@ -0,0 +1,84 @@
+﻿using Daisi.Protos.V1;
+using Daisi.SDK.Clients.V1.Host;
+using Daisi.SDK.Extensions;
+using Daisi.SDK.Models;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Console = System.Console;
+
+namespace Daisi.Console.Chat
+{
+    internal class DaisiChatBot(InferenceClientFactory inferenceClientFactory) : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            System.Console.WriteLine($"Loading Chat Session with {DaisiStaticSettings.OrcUrl}");
+
+
+            // Create the client that we will use to connect to the
+            // Daisi network and communicate with the host
+            var inferenceClient = inferenceClientFactory.Create();
+
+
+            // OPTIONAL: Create the inference session with an initialization prompt.
+            // This isn't strictly necessary and the client will do it if
+            // you don't call it manually. Uncomment to use this option.
+
+            //var inferenceSession = await inferenceClient.CreateAsync(new CreateInferenceRequest()
+            //{
+            //    InitializationPrompt = "You are a friendly Daisi chatbot helping a new user understand the world.",
+            //    ThinkLevel = ThinkLevels.ThinkBasic                
+            //});
+
+            // Give the user some general instructions
+            System.Console.WriteLine($"Welcome to DaisiBot! Ask a question or type \"exit\" to stop.\n\n");
+
+            System.Console.ForegroundColor = ConsoleColor.Yellow;
+            System.Console.Write("User: ");
+            System.Console.ForegroundColor = ConsoleColor.White;
+
+            // Keep the chat going until the user wants to stop or the application stops
+            while (!stoppingToken.IsCancellationRequested) 
+            {
+
+                //Take the user's input              
+                string inputFromUser = System.Console.ReadLine();
+
+                //Make sure the user doesn't want to quit.
+                if (inputFromUser != "exit" && !string.IsNullOrWhiteSpace(inputFromUser))
+                {
+                    // Send the user's input to DAISI. In this case, we are
+                    // letting the Orc (Orchestrator) and the client figure everything out.
+                    var response = inferenceClient.Send(inputFromUser);
+
+                    System.Console.ForegroundColor = ConsoleColor.Green;
+                    System.Console.Write($"\nAssistant: ");
+                    System.Console.ForegroundColor = ConsoleColor.White;
+
+                    // Cycle through each part of the response until there are no 
+                    // more parts in the response and write each part to the console.
+                    while (await response.ResponseStream.MoveNext(stoppingToken))
+                    {
+                        System.Console.Write(response.ResponseStream.Current.Content.CleanupAssistantResponse());
+                    }
+                    System.Console.WriteLine();
+                    // Ask the user for more input
+                    System.Console.ForegroundColor = ConsoleColor.Yellow;
+                    System.Console.Write($"\nUser: ");
+                    System.Console.ForegroundColor = ConsoleColor.White;
+
+                }
+                else
+                    break;
+            }
+
+            // Closes the inference and logs everything appropriately.
+            // The inference session will timeout if you don't close it like this.
+            // Each timeout is a small negative on reputation, but they add up,
+            // so also be sure to close out your client requests.
+            await inferenceClient.CloseAsync();
+        }
+    }
+}
---
+++ Daisi.Console.Chat/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using Daisi.Console.Chat;
+using Daisi.Protos.V1;
+using Daisi.SDK.Clients.V1.Host;
+using Daisi.SDK.Clients.V1.Orc;
+using Daisi.SDK.Extensions;
+using Daisi.SDK.Interfaces;
+using Daisi.SDK.Models;
+using Grpc.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+Console.WriteLine("=================================");
+Console.WriteLine("Example: Hello, Daisi!");
+Console.WriteLine("Written by David Graham on 12/4/2025");
+Console.WriteLine("Copyright © 2025. Distributed AI Systems, Inc.");
+Console.WriteLine("=================================");
+
+var builder = Microsoft.Extensions.Hosting.Host.CreateApplicationBuilder(new HostApplicationBuilderSettings()
+{
+    Args = args,
+    EnvironmentName = Environments.Development
+});
+
+/*
+ * Before you run the app, you need to add your secret key to your local user-secrets,
+ * To do so, execute the follow in the DotNet CLI, using your real secret key:
+ * 
+ * dotnet user-secrets set "Daisi:SecretKey" "<<Your Secret Key>>"
+ * 
+ * More Info: https://learn.microsoft.com/en-us/aspnet/core/security/app-secrets?view=aspnetcore-10.0&tabs=windows
+ * 
+ */
+
+// Add the default Daisi setup to the system, including clients, factories, 
+// default Orc and DefaultClientKeyProvider
+string secretKey = builder.Configuration["Daisi:SecretKey"];
+builder.Services.AddDaisi(secretKey);
+
+// Register the service that will actually run our chats with the hosts.
+builder.Services.AddHostedService<DaisiChatBot>();
+
+var host = builder.Build();
+
+// Sets up the app's Client Key for communicating with the Orc.
+host.Services.UseDaisi();
+
+await host.RunAsync();
+
